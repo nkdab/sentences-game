ad904e4b314687b61af868a18c45452499692ae9
Aleksandr Ivanov <a.ivanov.tmn@mail.ru>
2020-02-21 16:43:21 +0500

    refactor:  state in redux store

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import "./App.scss";
 import GameBoard from "../GameBoard/GameBoard";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { gameReducer } from "../../reducers/gameReducer";
+
+const store = createStore(gameReducer);
 
 function App() {
   return (
-    <div className="App">
-      <GameBoard />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <GameBoard />
+      </div>
+    </Provider>
   );
 }
 
--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,70 +1,13 @@
 import React from "react";
-import { useReducer } from "react";
 import PlayArea from "../PlayArea/PlayArea";
-
-const initialState = {
-  questionData: [
-    {
-      id: 1,
-      title: "what",
-    },
-    {
-      id: 2,
-      title: "are",
-    },
-    {
-      id: 3,
-      title: "you",
-    },
-    {
-      id: 4,
-      title: "doing",
-    },
-    {
-      id: 5,
-      title: "?",
-    },
-  ],
-  answerData: [],
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "MOVE_TO_ANSWER":
-      return {
-        answerData: [
-          ...state.answerData,
-          state.questionData.find(item => item.id === action.payload.id),
-        ],
-        questionData: state.questionData.filter(
-          item => item.id !== action.payload.id,
-        ),
-      };
-    // const data = state.questionData;
-    // const answerData = [...state.answerData, data[action.payload.id]];
-    // const questionData = data.filter(item => item.id !== action.payload.id);
-    // return { answerData, questionData };
-    case "MOVE_TO_QUESTION":
-      return {
-        questionData: [
-          ...state.questionData,
-          state.answerData.find(item => item.id === action.payload.id),
-        ],
-        answerData: state.answerData.filter(
-          item => item.id !== action.payload.id,
-        ),
-      };
-    // const data1 = state.answerData;
-    // const questionData1 = [...state.questionData, data1[action.payload.id]];
-    // const answerData1 = data1.filter(item => item.id !== action.payload.id);
-    // return { answerData: answerData1, questionData: questionData1 };
-    default:
-      return state;
-  }
-};
+import { useSelector, useDispatch } from "react-redux";
 
 const GameBoard = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  // const [state, dispatch] = useReducer(reducer, initialState);
+
+  const answerData = useSelector(state => state.answerData);
+  const questionData = useSelector(state => state.questionData);
+  const dispatch = useDispatch();
 
   const handleAnswerItemClick = (e, id) => {
     e.preventDefault();
@@ -76,11 +19,8 @@ const GameBoard = () => {
   };
   return (
     <div>
-      <PlayArea items={state.answerData} onItemClick={handleAnswerItemClick} />
-      <PlayArea
-        items={state.questionData}
-        onItemClick={handleQuestionItemClick}
-      />
+      <PlayArea items={answerData} onItemClick={handleAnswerItemClick} />
+      <PlayArea items={questionData} onItemClick={handleQuestionItemClick} />
     </div>
   );
 };
